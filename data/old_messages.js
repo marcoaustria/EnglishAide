a3aad33c2c01a9088446ef49bd06c2032e6fba67
Gianne <gianneflores18@gmail.com>
2019-01-20 06:33:37 -0500

    edit style

--- data/old_messages.js
+++ data/old_messages.js
@@ -2,26 +2,11 @@ module.exports = [
   {
     _id: Math.round(Math.random() * 1000000),
     text:
-      "It uses the same design as React, letting you compose a rich mobile UI from declarative components https://facebook.github.io/react-native/",
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+      "Oh, I get it now. Thank you!",
+    createdAt: new Date(Date.UTC(2019, 0, 10, 17, 20, 0)),
     user: {
       _id: 1,
       name: "Developer"
     }
-  },
-  {
-    _id: Math.round(Math.random() * 1000000),
-    text: "React Native lets you build mobile apps using only JavaScript",
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
-    user: {
-      _id: 1,
-      name: "Developer"
-    }
-  },
-  {
-    _id: Math.round(Math.random() * 1000000),
-    text: "This is a system message.",
-    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
-    system: true
   }
 ];
